e810f53eb4af8f41d1a5aa6b52de2f23e08663b8
Marcos A. Verdura <mav.unlam@gmail.com>
2024-10-27 20:44:47 -0300

    CoinListItem finished

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
@@ -75,6 +77,10 @@ fun CoinListItem(
                 fontSize = 16.sp,
                 fontWeight = FontWeight.SemiBold,
                 color = contentColor
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            PriceChange(
+                change = coinUi.changePercent24Hr
             )
         }
     }
